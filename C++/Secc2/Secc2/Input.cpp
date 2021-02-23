6bc3fbb84f44d8fac91d48600c817a2e11140b3e
DieguitoPerez <56733417+DieguitoPerez@users.noreply.github.com>
2021-02-23 12:08:16 -0600

    Add files via upload

---
+++ C++/Secc2/Secc2/Input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"Inserta tu nombre:" <<flush;
+    string entrada;
+    cin >> entrada;
+    cout <<"Estas enterado" <<entrada <<endl;
+
+
+    cout <<"Inserta un numero:" << flush;
+    int salida;
+    cin >> salida;
+    cout << "Estas enterado" <<salida << endl;
+
+    return 0;
+
+
+}
